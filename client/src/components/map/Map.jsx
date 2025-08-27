244980510c47a282d45882e2679cd5884809b417
Rhythm Jain <124446653+rhythmjain1@users.noreply.github.com>
2025-08-27 16:20:34 +0530

    Updates

--- client/src/components/map/Map.jsx
+++ client/src/components/map/Map.jsx
@@ -18,7 +18,7 @@ function Map({ items }) {
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
+      /> 
       {items.map((item) => (
         <Pin item={item} key={item.id} />
       ))}
--- client/src/components/pin/Pin.jsx
+++ client/src/components/pin/Pin.jsx
@@ -2,7 +2,7 @@ import { Marker, Popup } from "react-leaflet";
 import "./pin.scss";
 import { Link } from "react-router-dom";
 
-function Pin({ item }) {
+function Pin({ item }) { 
   return (
     <Marker position={[item.latitude, item.longitude]}>
       <Popup>
